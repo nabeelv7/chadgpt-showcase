0ae831c6a3308e81b39568d5be4c650f06bb4255
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-16 15:35:14 +0500

    done

--- app/page.js
+++ app/page.js
@@ -1,5 +1,57 @@
+import Image from "next/image";
+import steps from "./steps";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1>ChadGPT Showcase</h1>
+    <>
+      <main className="flex flex-col gap-5 justify-center items-center py-20 max-w-screen-lg mx-auto px-5">
+        <h1 className="text-3xl font-bold">ChadGPT Showcase</h1>
+
+        {steps.map((step) => {
+          if (step.type == "image") {
+            return <StepImage key={step.id} link={step.link} id={step.id} />
+          } else {
+            return <StepVideo key={step.id} link={step.link} id={step.id} />
+          }
+        })}
+      </main>
+      <footer className="flex justify-center items-center pb-10">
+        <h1>website by&nbsp;
+          <a href="https://github.com/nabeelv7" target="_blank" className="underline">nabeel</a>
+        </h1>
+      </footer>
+    </>
   );
+}
+
+export function StepImage({ link, id }) {
+  return (
+    <div>
+      <div className="flex items-center justify-between py-5 w-full">
+        <h1 className="text-2xl font-medium">{id}.</h1>
+        <Button asChild>
+          <a href={link} download>Download</a>
+        </Button>
+      </div>
+      <Image src={link} width={1920} height={1080} alt={`Step number ${id}`} />
+    </div>
+  )
+}
+
+export function StepVideo({ link, id }) {
+  return (
+    <div>
+      <div className="flex items-center justify-between py-5 w-full">
+        <h1 className="text-2xl font-medium">{id}.</h1>
+        <Button asChild>
+          <a href={link} download>Download</a>
+        </Button>
+      </div>
+      <video controls width={1920} height={1080}>
+        <source src={link} />
+        Your browser does not support the html video tag
+      </video>
+    </div>
+  )
 }
---
+++ app/steps.js
@@ -0,0 +1,90 @@
+const steps = [
+    {
+        id: 1,
+        type: "image",
+        link: "/1.png",
+    },
+    {
+        id: 2,
+        type: "image",
+        link: "/2.png",
+    },
+    {
+        id: 3,
+        type: "image",
+        link: "/3.png",
+    },
+    {
+        id: 4,
+        type: "image",
+        link: "/4.png",
+    },
+    {
+        id: 5,
+        type: "image",
+        link: "/5.png",
+    },
+    {
+        id: 6,
+        type: "image",
+        link: "/6.png",
+    },
+    {
+        id: 7,
+        type: "image",
+        link: "/7.png",
+    },
+    {
+        id: 8,
+        type: "video",
+        link: "/8.mp4",
+    },
+    {
+        id: 9,
+        type: "image",
+        link: "/9.png",
+    },
+
+    {
+        id: 10,
+        type: "image",
+        link: "/10.png",
+    },
+    {
+        id: 11,
+        type: "image",
+        link: "/11.png",
+    },
+    {
+        id: 12,
+        type: "video",
+        link: "/12.mp4",
+    },
+    {
+        id: 13,
+        type: "image",
+        link: "/13.png",
+    },
+    {
+        id: 14,
+        type: "image",
+        link: "/14.png",
+    },
+    {
+        id: 15,
+        type: "image",
+        link: "/15.png",
+    },
+    {
+        id: 16,
+        type: "image",
+        link: "/16.png",
+    },
+    {
+        id: 17,
+        type: "image",
+        link: "/17.png",
+    },
+]
+
+export default steps;
