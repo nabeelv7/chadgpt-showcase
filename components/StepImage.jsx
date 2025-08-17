8d15f84cc7048385a4f26e7d234d31dad7989dfc
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-17 13:02:16 +0500

    update: removed client side loading

--- components/StepImage.jsx
+++ components/StepImage.jsx
@@ -1,12 +1,11 @@
-"use client";
-
+// "use client";
 import Image from "next/image";
-import { Skeleton } from "./ui/skeleton";
+// import { Skeleton } from "./ui/skeleton";
 import { Button } from "./ui/button";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function StepImage({ link, id }) {
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
     return (
         <div>
@@ -18,25 +17,26 @@ export default function StepImage({ link, id }) {
             </div>
 
             <div className="relative w-full aspect-video">
-                {loading && (
+                {/* {loading && (
                     <div className="absolute inset-0">
                         <ImageSkeleton />
                     </div>
-                )}
+                )} */}
                 <Image
                     src={link}
                     alt={`Step number ${id}`}
                     width={1920}
                     height={1080}
-                    className={`object-cover rounded transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"
-                        }`}
-                    onLoad={() => setLoading(false)}
+                    className="object-cover rounded transition-opacity duration-500"
+                // className={`object-cover rounded transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"
+                //     }`}
+                // onLoad={() => setLoading(false)}
                 />
             </div>
         </div>
     );
 }
 
-export function ImageSkeleton() {
-    return <Skeleton className="w-full h-full rounded" />;
-}
+// export function ImageSkeleton() {
+//     return <Skeleton className="w-full h-full rounded" />;
+// }
